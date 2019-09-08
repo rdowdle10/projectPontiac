309862f576f5f180df6185baa9b785f766e1d990
Richard Dowdle <rdowdle10@gmail.com>
2019-09-08 00:19:30 -0700

    Fixed the mixed up descriptions of both the Blocked Traffic and Special Events fields. Also relocated Volume slider farther away from playback buttons due to overlapping touch events.

--- start.sh
+++ start.sh
@@ -10,9 +10,9 @@ rm specialevents
 rm volumelevel
 
 # Filter information from output and write to blockedtraffic file
-cat default.aspx | grep -i -A 2 BlockingU | grep li | cut -b 29-500 | sed "s/'</li>'/" >> blockedtraffic
+cat default.aspx | grep -i -A 2 BlockingU | grep li | cut -b 29-500 | sed "s/'</li>'/" >> specialevents
 # Filter information from output and write to specialevents file
-cat default.aspx | grep -i -A 2 SpecialU | grep li | cut -b 29-500 | sed "s/'</li>'/" >> specialevents
+cat default.aspx | grep -i -A 2 SpecialU | grep li | cut -b 29-500 | sed "s/'</li>'/" >> blockedtraffic
 # Download the map for traffic
 wget -q https://images.wsdot.wa.gov/traffic/flowmaps/tacoma.png -O tacoma.png
 
